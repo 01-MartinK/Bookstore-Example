4ffa618d5b0da98ad16d8c5bb6f3c0c7a505cbc8
RobinRattasep <rattasep.robin@voco.ee>
2023-02-14 10:34:22 +0200

    Andmebaasi ühendus

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const path = require('path');
+const sequelize = require('./database');
 const bodyParser = require('body-parser')
 const pug = require('pug')
 const PORT = 3001
@@ -9,8 +10,17 @@ const PORT = 3001
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, '/views'));
 
-app.get('/', (req, res) => {
-    res.render('template')
+app.get('/', async (req, res) => {
+    try {
+        const raamat = await sequelize.query('SELECT * FROM raamat', { type: sequelize.QueryTypes.SELECT });
+        console.log(raamat)
+        console.log("test3")
+        res.render('template')
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while trying to select * from hotell' });
+    }
 })
 
 const server = app.listen(PORT, () => {
