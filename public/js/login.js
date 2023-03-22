792e4951e496cd569a2ab3cb0ec12a93061c4d0f
MK <46935118+TheDeadGamer101@users.noreply.github.com>
2023-03-22 22:43:57 +0200

    Register Visualization Implementation

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,27 @@
 let signInBtn = document.querySelector('#signInBtn');
 signInBtn.addEventListener('click', showLoginBox);
+let registerShowBtn = document.querySelector('#signUpButtonLink');
+registerShowBtn.addEventListener('click', showRegisterBox)
+document.querySelector('#signInButtonLink').addEventListener('click', showLoginBox)
 
 document.querySelector('.loginBox').children[1].addEventListener('submit', submitLogin)
 
+document.querySelector('.registerBox').children[1].addEventListener('submit', submitRegister)
+
 function showLoginBox() {
+    let registerBox = document.querySelector('.registerBox')
+    registerBox.style.display = 'none';
     let loginBox = document.querySelector('.loginBox')
     loginBox.style.display = 'flex';
-    setTimeout(() => { hideOnClickOutside(document.querySelector('.loginBox')) }, 100)
+    setTimeout(() => { hideOnClickOutside(loginBox) }, 500)
+}
+
+function showRegisterBox() {
+    let loginBox = document.querySelector('.loginBox')
+    loginBox.style.display = 'none'
+    let registerBox = document.querySelector('.registerBox')
+    registerBox.style.display = 'flex';
+    setTimeout(() => { hideOnClickOutside(registerBox) }, 500)
 }
 
 function hideOnClickOutside(element) {
@@ -24,12 +39,26 @@ function hideOnClickOutside(element) {
     document.addEventListener('click', outsideClickListener);
 }
 
+// submit login
 function submitLogin(e) {
     e.preventDefault()
     let name = e.target.children[0].lastChild.value
     let password = e.target.children[3].lastChild.value
     
     login(name, password)
+}
+
+// submit register
+function submitRegister(e) {
+    e.preventDefault()
+    let name;
+    let email;
+    let telefon;
+    let isikukood;
+    let password;
+    let aadress;
+
+    registration(name, isikukood, password, email, aadress, telefon)
 }
 
 const isVisible = elem => !!elem && !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length );
